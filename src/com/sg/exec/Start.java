8ea119a65942960b4c59b800cac169ce42699c22
nitsu <augustin_jacques@yahoo.fr>
2021-08-31 17:36:50 +0200

    1st commit

---
+++ src/com/sg/exec/Start.java
@@ -0,0 +1,13 @@
+package com.sg.exec;
+
+/**
+ *
+ * @author Jacques
+ */
+public class Start {
+    public static void main(String[] args) {
+        // un "main" épuré et minimaliste..
+        Menu monMenu = new Menu();
+        monMenu.gererMenu();
+    }
+}
